f755935104cd05b9f44791badb7259891f7399fb
Sven Haardiek <sven@haardiek.de>
2024-01-18 21:59:18 +0100

    very simple evaluation

---
+++ pkg/position/evaluation.go
@@ -0,0 +1,30 @@
+package position
+
+import (
+	"github.com/shaardie/clemens/pkg/types"
+)
+
+const (
+	kingScalar   = 200
+	queenScalar  = 9
+	rookScalar   = 5
+	bishopScalar = 3
+	knightScalar = 3
+	pawnScalar   = 1
+)
+
+func (pos *Position) Evaluation() float64 {
+	materialScore :=
+		kingScalar*(pos.piecesBitboard[types.WHITE][types.KING].PopulationCount()-pos.piecesBitboard[types.BLACK][types.KING].PopulationCount()) +
+			queenScalar*(pos.piecesBitboard[types.WHITE][types.QUEEN].PopulationCount()-pos.piecesBitboard[types.BLACK][types.QUEEN].PopulationCount()) +
+			rookScalar*(pos.piecesBitboard[types.WHITE][types.ROOK].PopulationCount()-pos.piecesBitboard[types.BLACK][types.ROOK].PopulationCount()) +
+			bishopScalar*(pos.piecesBitboard[types.WHITE][types.BISHOP].PopulationCount()-pos.piecesBitboard[types.BLACK][types.BISHOP].PopulationCount()) +
+			knightScalar*(pos.piecesBitboard[types.WHITE][types.KNIGHT].PopulationCount()-pos.piecesBitboard[types.BLACK][types.KNIGHT].PopulationCount()) +
+			pawnScalar*(pos.piecesBitboard[types.WHITE][types.PAWN].PopulationCount()-pos.piecesBitboard[types.BLACK][types.PAWN].PopulationCount())
+
+	if pos.sideToMove == types.BLACK {
+		materialScore *= -1
+	}
+
+	return float64(materialScore)
+}
