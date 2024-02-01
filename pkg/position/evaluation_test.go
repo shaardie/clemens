e179d45c2c71043834a2d548eb70d91c17ecf383
Sven Haardiek <sven@haardiek.de>
2024-02-01 20:45:07 +0100

    Fix eval test

--- pkg/position/evaluation_test.go
+++ pkg/position/evaluation_test.go
@@ -15,12 +15,12 @@ func TestPosition_Evaluation(t *testing.T) {
 		{
 			name:                "from white",
 			fen:                 "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1",
-			evalutationOverZero: true,
+			evalutationOverZero: false,
 		},
 		{
 			name:                "from black",
 			fen:                 "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 b kq - 0 1",
-			evalutationOverZero: false,
+			evalutationOverZero: true,
 		},
 	}
 	for _, tt := range tests {
