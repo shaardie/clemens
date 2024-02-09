4266508b4bd7268f7b1efc28d2af9fdaddd2fd0e
Sven Haardiek <sven@haardiek.de>
2024-02-09 16:15:48 +0100

    Update elo

--- scripts/elo.sh
+++ scripts/elo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -eux
-cutechess-cli -tournament gauntlet -concurrency 4 -pgnout output_pgn_file.pgn \
+cutechess-cli -tournament gauntlet -concurrency 4 -pgnout /save/output_pgn_file.pgn \
     -engine cmd=clemens st=1 \
     -engine name=maia-1100 cmd=lc0 arg=-w arg=/models/maia-1100.pb.gz nodes=1 st=1 \
     -engine name=maia-1200 cmd=lc0 arg=-w arg=/models/maia-1200.pb.gz nodes=1 st=1 \
@@ -14,6 +14,5 @@ cutechess-cli -tournament gauntlet -concurrency 4 -pgnout output_pgn_file.pgn \
     -engine name=maia-1900 cmd=lc0 arg=-w arg=/models/maia-1900.pb.gz nodes=1 st=1 \
     -each proto=uci -draw movenumber=40 movecount=4 score=8 \
     -resign movecount=4 score=500 \
-    -openings file=/openings.pgn order=random -repeat -rounds 50 -games 2
-cat output_pgn_file
+    -openings file=/openings.pgn order=random -repeat -rounds 100 -games 2
 ordo -Q -D -a 0 -A "maia-1100" -W -n8 -s1000 -U "0,1,2,3,4,5,6,7,8,9,10" -p output_pgn_file.pgn
