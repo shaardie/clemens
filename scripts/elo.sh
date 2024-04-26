c15d892cd2e900068d5d3e6d980130290afe6bff
Sven Haardiek <sven.haardiek@uni-muenster.de>
2024-04-26 08:47:43 +0200

    less time for stockfish, it does not matter

--- scripts/elo.sh
+++ scripts/elo.sh
@@ -5,7 +5,7 @@ ELO="2000"
 
 cutechess-cli -tournament gauntlet -concurrency 3 -pgnout output_pgn_file.pgn \
     -engine cmd=clemens st=0.1 \
-    -engine name=stockfish cmd=stockfish option.UCI_LimitStrength=true "option.UCI_Elo=$ELO" tc=40/1+0.1 \
+    -engine name=stockfish cmd=stockfish option.UCI_LimitStrength=true "option.UCI_Elo=$ELO" tc=40/1+0.05 \
     -each proto=uci -draw movenumber=40 movecount=4 score=8 \
     -resign movecount=4 score=500 \
     -openings file=/openings.pgn order=random -repeat -rounds 400 -games 2
